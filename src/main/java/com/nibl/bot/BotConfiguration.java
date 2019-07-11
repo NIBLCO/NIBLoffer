2cfd9b29293aa4459e5770e307a1e05a828c2b32
Rob P <jenga201@gmail.com>
2019-07-10 20:49:49 -0400

    Some local changes. I dunno what it is

--- src/main/java/com/nibl/bot/BotConfiguration.java
+++ src/main/java/com/nibl/bot/BotConfiguration.java
@@ -1,5 +1,8 @@
 package com.nibl.bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.net.ssl.SSLSocketFactory;
 
 import org.pircbotx.Configuration;
@@ -10,39 +13,39 @@ import com.nibl.bot.listener.Listener;
 
 public class BotConfiguration {
 
-    private BotConfiguration() {
+	private BotConfiguration() {
 
-    }
+	}
 
-    public static Configuration createConfiguration(Bot myBot) {
+	public static Configuration createConfiguration(Bot myBot) {
 
-        Builder cb = new Builder();
-        cb.setName("FileBoyz");
-        cb.addAutoJoinChannel("#frog");
-        cb.setVersion("GomuGomu");
-        cb.setFinger("Watch where you are poking");
-        cb.setLogin("FileBoy");
-        cb.addServer("irc.rizon.net", 9999);
-        cb.addListener(new Listener(myBot).getAdapter());
+		Builder cb = new Builder();
+		cb.setName("FileBoyz");
+		cb.addAutoJoinChannel("#frog");
+		cb.setVersion("GomuGomu");
+		cb.setFinger("Watch where you are poking");
+		cb.setLogin("FileBoy");
+		cb.addServer("irc.rizon.net", 9999);
+		cb.addListener(new Listener(myBot).getAdapter());
 
-        cb.setMessageDelay(new AdaptingDelay(100, 500));
-        cb.setAutoNickChange(true);
-        cb.setAutoReconnect(true);
-        cb.setSocketFactory(SSLSocketFactory.getDefault());
-        // List<Integer> dccPorts = new ArrayList<>();
-        // dccPorts.add(47470);
-        // dccPorts.add(47471);
-        // dccPorts.add(47472);
-        // dccPorts.add(47473);
-        // cb.setDccPorts(dccPorts);
-        cb.setDccTransferBufferSize(1024 * 4);
-        // try {
-        // cb.setDccPublicAddress(InetAddress.getByName("116.203.134.128"));
-        // } catch (UnknownHostException e) {
-        // log.error("Unable to set DCC Public IP Address!!", e);
-        // }
+		cb.setMessageDelay(new AdaptingDelay(100, 500));
+		cb.setAutoNickChange(true);
+		cb.setAutoReconnect(true);
+		cb.setSocketFactory(SSLSocketFactory.getDefault());
+		List<Integer> dccPorts = new ArrayList<>();
+		dccPorts.add(47470);
+		// dccPorts.add(47471);
+		// dccPorts.add(47472);
+		// dccPorts.add(47473);
+		cb.setDccPorts(dccPorts);
+//		cb.setDccTransferBufferSize(1024 * 32);
+		// try {
+		// cb.setDccPublicAddress(InetAddress.getByName("116.203.134.128"));
+		// } catch (UnknownHostException e) {
+		// log.error("Unable to set DCC Public IP Address!!", e);
+		// }
 
-        return cb.buildConfiguration();
-    }
+		return cb.buildConfiguration();
+	}
 
 }
