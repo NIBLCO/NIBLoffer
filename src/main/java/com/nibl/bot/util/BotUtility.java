18de7213b3b3b07740c4a72953f1d612ff29a056
Rob P <jenga201@gmail.com>
2019-07-13 00:10:50 -0400

    Calculate human readable size in a compatible manner with ooinuza

--- src/main/java/com/nibl/bot/util/BotUtility.java
+++ src/main/java/com/nibl/bot/util/BotUtility.java
@@ -5,16 +5,19 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-
 public class BotUtility {
 
 	private BotUtility() {
 
 	}
 
-	public static String bytesToHuman(long size) {
-		return FileUtils.byteCountToDisplaySize(size);
+	public static String bytesToHuman(long bytes) {
+		int unit = 1000;
+		if (bytes < unit)
+			return bytes + " B";
+		int exp = (int) (Math.log(bytes) / Math.log(unit));
+		String pre = "kMGTPE".charAt(exp - 1) + "";
+		return String.format("%.1f%s", bytes / Math.pow(unit, exp), pre);
 	}
 
 	/**
